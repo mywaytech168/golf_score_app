61acd2c687fc7b84aa160c8d5e470a9d366acc3f
mywaytech168 <mywaytech@getmywaytech.com>
2025-10-15 16:19:02 +0800

    後端 - 修正 - Android 建置設定 - 升級 Kotlin 插件版本以排除 metadata 不相容

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,7 +19,10 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    // ---------- Kotlin 版本說明 ----------
+    // 套件像是 package_info_plus 依賴 Kotlin 2.x 標準函式庫，若仍使用 1.8.x 會觸發 metadata 不相容錯誤
+    // 因此統一升級至 2.0.21 版本，確保編譯時 Kotlin 編譯器與函式庫版本一致
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 include(":app")
